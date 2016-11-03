59afa6e3098b0651c7d642e07d76a895436d4584
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2016-11-02 18:10:04 -0700

    remove unused

--- deploy.sh
+++ deploy.sh
@@ -10,15 +10,8 @@ fi
 cp vimrc ~/.vimrc
 vim +BundleInstall +qall
 
-#mkdir build \
-    #&& cd build\
-    #&& cmake -G "Unix Makefiles" . "$YCM_DIR/third_party/ycmd/cpp" \
-    #&& cmake --build . --target ycm_core --config Release \
-    #|| echo "Skipping YCM. Build folder exists"
 
-cd ~/.vim/bundle/YouCompleteMe && ./install.py --racer-completer
-#if [ $(which rustc) ]; then
-    #cd "$YCM_DIR/third_party/ycmd/third_party/racerd" && cargo build --release
-    #git clone git@github.com:rust-lang/rust.git ~/.vim/rust_src
-#fi
+#cd ~/.vim/bundle/YouCompleteMe && ./install.py --racer-completer
+#git clone git@github.com:rust-lang/rust.git ~/.vim/rust_src
+
 
